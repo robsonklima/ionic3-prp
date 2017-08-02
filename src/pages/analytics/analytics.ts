40c1f1b6e450cbc317235a3b44b17396877551c6
robsonklima <robsonklima@gmail.com>
2017-08-01 21:04:58 -0300

    code ref

--- src/pages/analytics/analytics.ts
+++ src/pages/analytics/analytics.ts
@@ -141,11 +141,11 @@ export class AnalyticsPage {
           label: 'Amount',
           data: this.raValues,
           backgroundColor: [
-            'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'
+            'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'
           ],
           hoverBackgroundColor: [
-            "#FFCE56", "#FF6384", "#36A2EB", "#FFCE56", "#FF6384", "#36A2EB"
+            "#FF6384", "#36A2EB", "#FFCE56", "#FF6384", "#36A2EB", "#FFCE56"
           ]
         }]
       }
